42e8b16793275cdb5ea84f9171c8bdc21d76bc48
Anhad Khurana <anhadkhurana@Anhads-MacBook-Pro.local>
2025-06-19 02:19:06 +0530

    Made changes to map plotter and datafolder structure

--- geo/flight_map_plotter.py
+++ geo/flight_map_plotter.py
@@ -1,16 +1,27 @@
 import os
 import pandas as pd
 import folium
+from folium.plugins import MiniMap
+from folium.plugins import PolyLineTextPath
 
 # File paths
 current_dir = os.path.dirname(__file__)
+airports_csv_path = os.path.join(current_dir, "..", "data", "origin_destination_data.csv")
 flight_path_csv_path = os.path.join(current_dir, "..", "output/routes", "flight_path.csv")
 emissions_summary_csv_path = os.path.join(current_dir, "..", "output/emissions", "emissions_summary.csv")
 
 # Load data
+airports_df = pd.read_csv(airports_csv_path)
 flight_path_df = pd.read_csv(flight_path_csv_path)
 emissions_summary_df = pd.read_csv(emissions_summary_csv_path)
 emissions_summary_df = emissions_summary_df[emissions_summary_df["Phase"] != "Total"]
+
+# Getting Airport Coords
+origin_data = airports_df[airports_df["IATA"] == "JFK"]
+destin_data = airports_df[airports_df["IATA"] == "YYZ"]
+
+coord_origin = (float(origin_data["Latitude"].iloc[0]), float(origin_data["Longitude"].iloc[0]))
+coord_destin = (float(destin_data["Latitude"].iloc[0]), float(destin_data["Longitude"].iloc[0]))
 
 # Emission data per phase
 co_em = emissions_summary_df["CO Emissions (g)"].tolist()
@@ -18,13 +29,35 @@ nox_em = emissions_summary_df["NOx Emissions (g)"].tolist()
 hc_em = emissions_summary_df["HC Emissions (g)"].tolist()
 
 # Start point for map
-start_coords = (flight_path_df["Latitude"].iloc[0], flight_path_df["Longitude"].iloc[0])
-m = folium.Map(location=start_coords, zoom_start=6)
+m = folium.Map(location=coord_origin, zoom_start=6)
 
 # Add polyline for route
-folium.PolyLine(
-    list(zip(flight_path_df["Latitude"], flight_path_df["Longitude"])),
-    color='blue', weight=1, opacity=0.5,
+route = folium.PolyLine(
+    [coord_origin, coord_destin],
+    color='black', weight=1, opacity=0.5,
+).add_to(m)
+
+arrow_path = PolyLineTextPath(
+    route,
+    '➤',       # Arrow character
+    repeat=True,
+    offset=12,  # Distance between arrows (in pixels)
+    attributes={'fill': 'black', 'font-weight': 'bold', 'font-size': '8'}
+)
+m.add_child(arrow_path)
+
+# Add origin marker
+folium.Marker(
+    location=coord_origin,
+    popup="JFK Airport (Origin)",
+    icon=folium.Icon(color='blue', icon='plane-departure', prefix='fa')
+).add_to(m)
+
+# Add destination marker
+folium.Marker(
+    location=coord_destin,
+    popup="YYZ Airport (Destination)",
+    icon=folium.Icon(color='red', icon='plane-arrival', prefix='fa')
 ).add_to(m)
 
 # Scale factor for circle radius (meters per gram)
@@ -64,5 +97,7 @@ for idx, row in flight_path_df.iterrows():
         popup=f"HC: {hc_em[idx]:.1f} g"
     ).add_to(m)
 
+m.add_child(MiniMap(toggle_display=True))
+
 # Save to file
-m.save("output/flight_path_emissions_map.html")
+m.save("output/routes/flight_path_emissions_map.html")
